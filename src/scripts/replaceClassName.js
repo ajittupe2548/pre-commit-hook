ed701867e3733ff39d750034306620c78b618be3
cw-ajit <ajit@carwale.com>
2024-08-06 11:44:32 +0530

    Added: pre-commit-hook

---
+++ src/scripts/replaceClassName.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+// Function to replace 'ajit' with 'tupe' in className attributes
+function replaceInFile(filePath) {
+    const content = fs.readFileSync(filePath, "utf8");
+    const updatedContent = content.replace(
+        /className\s*=\s*["']([^"']*)ajit([^"']*)["']/g,
+        'className="$1tupe$2"'
+    );
+
+    if (content !== updatedContent) {
+        fs.writeFileSync(filePath, updatedContent, "utf8");
+        console.log(`Updated: ${filePath}`);
+    }
+}
+
+// Get the list of files from lint-staged
+const files = process.argv.slice(2);
+
+// Process each file
+files.forEach((filePath) => {
+    const absolutePath = path.resolve(filePath);
+    replaceInFile(absolutePath);
+});
