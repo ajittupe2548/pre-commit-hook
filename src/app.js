af0c6f49e337d95fad142f945c064442a513039c
Ajit Tupe <ajittupe2548@gmail.com>
2024-08-06 10:34:55 +0530

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from  './app.scss';
+
+const app = () => {
+    return (
+        <h1 className={styles.heading}>Learn WebPack!</h1>
+    )
+}
+
+export default app;
