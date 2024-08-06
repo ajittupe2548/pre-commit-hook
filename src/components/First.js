e3227648d517931697898304e99c10bf88afc87b
cw-ajit <ajit@carwale.com>
2024-08-06 12:03:37 +0530

    Added: Ajit className

--- src/components/First.js
+++ src/components/First.js
@@ -2,7 +2,7 @@ import React from "react";
 import { getThemeClasses } from "../utils";
 
 function First() {
-    return <div className={getThemeClasses(`color-teal`)}>First</div>;
+    return <div className="ajit">First</div>;
 }
 
 export default First;
