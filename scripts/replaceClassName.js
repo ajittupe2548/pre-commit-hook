ebde8a8aa186d62d681ce81f3c6f717dacf0e55c
cw-ajit <ajit@carwale.com>
2024-08-07 18:18:16 +0530

    Added: Comments

--- scripts/replaceClassName.js
+++ scripts/replaceClassName.js
@@ -4,14 +4,15 @@ const classMapping = require('./classMapping'); // Adjust the path as needed
 
 // Function to replace classes based on the mapping
 function replaceInFile(filePath) {
+    //  Reads the file at the given filePath synchronously and stores it as a string
     let content = fs.readFileSync(filePath, 'utf8');
 
-    // Regex to match getThemeClasses with template literals, including multiline
+    // regular expression to match getThemeClassesfunction calls with template literals, single quotes, or double quotes. Thegflag enables global matching, and thesflag allows the dot.` to match newlines.
     const regexGetThemeClasses = /getThemeClasses\s*\(\s*(`[^`]*`|'[^']*'|"[^"]*")\s*\)/gs;
 
-    // Replace classes within getThemeClasses
+    // Replace classes within getThemeClasses i.e. get instances of getThemeClasses(``) classes contains first matching group from above regex
     content = content.replace(regexGetThemeClasses, (match, classes) => {
-        // Replace each class within the template literal
+        // Replace each class within the template literal i.e. replace color-red with color-neutral-100
         const updatedClasses = classes.replace(/\b(color-\w+|bg-\w+|border-color-\w+)\b/g, (className) => {
             return classMapping[className.trim()] || className.trim();
         });
@@ -19,25 +20,17 @@ function replaceInFile(filePath) {
         return match.replace(classes, updatedClasses);
     });
 
-    // Regex to match class names with or without quotes, including leading/trailing spaces
-    const regexClassNames = /(?:className\s*=\s*|['"`;])\s*\b(color-\w+|bg-\w+|border-color-\w+)\b\s*(?=[\s'";`])/g;
-
-    // Replace standalone class names
-    content = content.replace(regexClassNames, (match, className) => {
-        const trimmedClassName = className.trim();
-        const newClassName = classMapping[trimmedClassName] || trimmedClassName;
-        return match.replace(trimmedClassName, newClassName);
-    });
-
     fs.writeFileSync(filePath, content, 'utf8');
     console.log(`Updated: ${filePath}`);
 }
 
 // Get the list of files from lint-staged
+// Gets the list of files from lint-staged, starting from the third argument because first two arguments are node and this script itself.
 const files = process.argv.slice(2);
 
 // Process each file
 files.forEach(filePath => {
+    // path.resolve convert relative paths to absolute. e.g. /src/script => d:/hook/src/script
     const absolutePath = path.resolve(filePath);
     replaceInFile(absolutePath);
 });
